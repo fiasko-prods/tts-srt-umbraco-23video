0a709b8147db5b2523b870a00bcd5a2cd54e97d7
fiasko-prods <maiov@regionsjaelland.dk>
2026-04-20 10:49:05 +0200

    Update SynchronizedReader_TTS.js

--- SynchronizedReader_TTS.js
+++ SynchronizedReader_TTS.js
@@ -1,7 +1,7 @@
 // =====================================================
 // SYNCHRONIZED SUBTITLE READER — UNIVERSAL TEMPLATE
 // Uses 23video postMessage API
-// Version: 1.24b
+// Version: 1.24bis
 // Author: Marco Iovane maiov@regionsjaelland.dk
 // =====================================================
 //
